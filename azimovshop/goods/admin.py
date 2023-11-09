364ce56c14ddbb722dcbc796950644575c0d4643
deepxshine <aevdok2800@gmail.com>
2023-11-09 18:35:28 +0300

    9.11

--- azimovshop/goods/admin.py
+++ azimovshop/goods/admin.py
@@ -2,10 +2,16 @@ from django.contrib import admin
 
 from .models import Product, ParametersInProduct, Category, Parameter
 
+class ParameterInline(admin.TabularInline):
+    model = ParametersInProduct
+    extra = 1
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('id', 'name', 'price', 'category', 'count')
+    list_filter = ('price', 'count', 'category')
+    search_fields = ('name',)
+    inlines = ParameterInline,
 
 @admin.register(Parameter)
 class ParameterAdmin(admin.ModelAdmin):
